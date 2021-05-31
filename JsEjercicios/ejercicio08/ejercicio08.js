012a0c3a34eda8f2a13bbe612f7990e10fc07750
sonson82 <soniapujol@hotmail.es>
2021-05-31 11:46:31 +0200

    31/05/2021 ejercicio 08

--- JsEjercicios/ejercicio08/ejercicio08.js
+++ JsEjercicios/ejercicio08/ejercicio08.js
@@ -59,7 +59,7 @@ let sumaEntradaTeclado = () => {
 /* ------------------------------------------------- */
 /* Ejercicio día  */
 
-//Ejemplo de Carlos sencillo:   https://carlosboniniklison.github.io/publico/ejercicios/xml/27-abr-xml.html
+//Ejemplo de Carlos:   https://carlosboniniklison.github.io/publico/ejercicios/xml/27-abr-xml.html
 
 let registrados = [];
 
@@ -104,126 +104,129 @@ function crearTablaConDatos(xml,fuente) {
 
 /* ------------------------------------------------- */
 
-  //Ejemplo de Carlos form+localstorage: https://carlosboniniklison.github.io/publico/ejercicios/xml/loginXML.html
-  /* 
-JavaScript / XML
-web o pagina: ejercicios de XML y JavaScript
-autor: Prof. Carlos Boni
-fecha: 25 mayo 2021
-resumen: lectura y carga en array bidimensional de un XML
+  //Ejemplo de Carlos: https://carlosboniniklison.github.io/publico/ejercicios/xml/loginXML.html
+ 
+  let usrIntentando = "";
+  let claveIntentando = "";
 
-sintesis:
-la lógica se basa en el uso de tres variables de tipo sessionStorage
-usuarioLogueado : si existe, su valor es el nombre de usuario en sesion
-usuarioIntentando y claveIntentando: si existen significa que debemos
-validarlas para crear usuarioLogueado si corresponde
-*/
-
-// variable global de la pagina
-
-// las siguientes variables estarán como sessionStorage
-let usrIntentando = "";
-let claveIntentando = "";
-
-function controlar(){
+  function controlar(){	
 	// determinamos en qué estado se carga la página:
-	// 1 - sin usuario
+	// 1 - usuario con sesion iniciada
 	// 2 - usuario intentando ingresar
-	// 3 - usuario con sesion iniciada
-	$("#ingresar").show();
-	$("#desconectar").hide();
-	for(let timer=1;timer<5000000;timer++);	
-			
-	if (sessionStorage.getItem("usuarioLogueado")) {
-		// estado 3 de nuestro diagrama de estados - con usuario
-		// estamos cargando la página teniendo un usuario logueado previamente
-		// y con la sesión activa pues no se ha desconectado aún
-		// ocultamos formulario de login y mostramos desconectar
-		$("#ingresar").hide();
+	// 3 - sin usuario 
+	let divSaldiaForm = document.getElementById("salidaForm");
+	let message = null;
+
+	if (sessionStorage.getItem("usuarioLogueado")) { // 1 - usuario con sesion iniciada
+		$("#formulario").hide();
+		//$("#limpiar").hide();
 		$("#desconectar").show();	
-		
-	} else {
-		if (sessionStorage.getItem("usuarioIntentando")) {
-			// estado 2 de nuestro diagrama de estados - transición
-			// estamos recargando luego de que haya un intento de login
-			// debemos validar si el usuario existe
-			validarXML();
-			// tardo un poco en recargar para dar tiempo a AJAX?
-			for(let timer=1;timer<5000000;timer++);
-			$("#ingresar").hide();
-			$("#desconectar").show();	
+
+		message = "Hola " + sessionStorage.getItem("usuarioLogueado");
+
+	} else { // Si no existe un usuario con sesion iniciada
+		$("#formulario").show();
+		//$("#limpiar").show();
+		$("#desconectar").hide();
+
+		if (sessionStorage.getItem("usuarioIntentando")) { // 2 - usuario intentando ingresar
+			conectarXML();
 			
-		} else {
-			// estado 1 de nuestro diagrama de estados - sin usuario
-			// mostramos formulario de login y ocultamos desconectar
-			$("#ingresar").show();
-			$("#desconectar").hide();
-		}
+		} else { // 3 - sin usuario 
+
+			if(sessionStorage.getItem("isValidated")){ //Si ha habido intento de validación
+				message = "Lo sentimos, usuario y contraseña incorrectos...";
+				sessionStorage.removeItem("isValidated");
+			} else { //Sin intento de validación
+				message = "Indica usuario y contraseña... ";
+			}
+		}		
+	}
+
+	divSaldiaForm.innerHTML = message;
+}
+
+
+  function intentar(){
+	// Guardamos datos intento de validación
+	// Al recargar la página, los tendremos en la memoria del navegador.
+	  
+	let divSaldiaForm = document.getElementById("salidaForm");
+
+	if (typeof(Storage) !== "undefined") {
+
+		sessionStorage.setItem("usuarioIntentando", document.forms["miFormulario"]["formUsuario"].value);
+		sessionStorage.setItem("claveIntentando", document.forms["miFormulario"]["formClave"].value);
+
+	} else {
+		divSaldiaForm.innerHTML = "Este navegador no soporta web storage...";
 	}
 }
-	
-	function intentar(){
-		if (typeof(Storage) !== "undefined") {
-		  
-		  // oculta la opción de login 
-		  $("#ingresar").hide();
-		  
-		  // Almacena un valor usando el método setItem del objeto localStorage
-		  var x=document.forms["miFormulario"]["formUsuario"].value;
-		  var y=document.forms["miFormulario"]["formClave"].value;
-		  sessionStorage.setItem("usuarioIntentando", x);
-		  sessionStorage.setItem("claveIntentando", y);
-		  
-		  // ya tengo en memoria webStorage lo que puso en el formulario
-		  // al recargarse la página podré recordar esta información
 
-		} else {
-		  document.getElementById("mensaje").innerHTML = "Este navegador no soporta web storage...";
+function conectarXML() {
+	//Conectamos con el archivo XML (desde github)
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			validarUsrPsw(this);
 		}
-	}
+	};
+	xhr.open("GET", "https://carlosboniniklison.github.io/publico/ejercicios/xml/registrados.xml", true);
+	xhr.send();
+}
+
+function validarUsrPsw(xml) {
+	//Validaremos los datos guardados en la memoria (temporales: introducidos por el usuario en el formulario)
+	//contrastando con los datos XML
+
+	usuarioIntentando=sessionStorage.getItem("usuarioIntentando");
+	claveIntentando=sessionStorage.getItem("claveIntentando");
+
+	var i = 0;
+	var xmlDoc = xml.responseXML;
+	var x = xmlDoc.getElementsByTagName("usuario");
+	let isValidated = false;
 	
-	function validarXML() {
-		
-		// lee desde aquí.
-		usuarioIntentando=sessionStorage.getItem("usuarioIntentando");
-		claveIntentando=sessionStorage.getItem("claveIntentando");
-		sessionStorage.removeItem("usuarioIntentando");
-		sessionStorage.removeItem("claveIntentando");
-		
-		var xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				miFuncion(this);
-			}
-		};
-		xhr.open("GET", "https://carlosboniniklison.github.io/publico/ejercicios/xml/registrados.xml", true);
-		xhr.send();
+	while ((isValidated == false) && ( i < x.length)){
+
+		if ((x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue == usuarioIntentando) && (x[i].getElementsByTagName("clave")[0].childNodes[0].nodeValue == claveIntentando)) {	
+			isValidated = true;
+		  } 
+
+		i++;
 	}
 
-	function miFuncion(xml) {
-	  var i;
-	  var usrNom;
-	  var usrPsw;
-	  var xmlDoc = xml.responseXML;
-	  var x = xmlDoc.getElementsByTagName("usuario");
-	  sessionStorage.removeItem("usuarioLogueado");
-	  
-	  for (i = 0; i <x.length; i++) { 
-		// leo las etiquetas que me interesan del objeto
-		usrNom = x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
-		usrPsw = x[i].getElementsByTagName("clave")[0].childNodes[0].nodeValue;
-		// actualizo la tabla de visualización
-		if ((usrNom == usuarioIntentando) && (usrPsw == claveIntentando)) {
-		  // destaca el usuario que coincide con lo que buscamos
-		  sessionStorage.setItem("usuarioLogueado",usuarioIntentando);
-		}
-	  }
-	}
-
-	function desconectar(){
+	//Las siguientes variables se leeran al refrescar la página en la función controlar().
+	if(isValidated){ 
+		// En caso de ser correctos (user and pass),
+		// almacenamos el usuario en la variable usuarioLogueado de la memoria.
+		sessionStorage.setItem("usuarioLogueado",usuarioIntentando);
+	} else { 
+		// En caso erroneo, 
+		// eliminamos la variable usuarioLogueado de la memoria.
 		sessionStorage.removeItem("usuarioLogueado");
-		sessionStorage.removeItem("usuarioIntentando");
-		sessionStorage.removeItem("claveIntentando");
-		window.location.reload();
 	}
-	
+
+	sessionStorage.setItem("isValidated", isValidated);
+
+	//Eliminamos las variables temporales (intento de login)
+	sessionStorage.removeItem("usuarioIntentando");
+	sessionStorage.removeItem("claveIntentando");
+	// Llamamos a la función principal controladora del estado de variables. En vez de forzar la recarga la página
+	controlar();
+}
+
+function desconectar(){
+	//Limpiamos campos de formulario
+	document.forms["miFormulario"]["formUsuario"].value == "";
+	document.forms["miFormulario"]["formClave"].value == "";
+	// Eliminamos todas las variables de la memoria del navegador.
+	sessionStorage.removeItem("usuarioLogueado");
+	sessionStorage.removeItem("isValidated");
+	sessionStorage.removeItem("usuarioIntentando");
+	sessionStorage.removeItem("claveIntentando");
+	// Forzamos la recarga de la página
+	window.location.reload();
+}
+
+ 
